ee021f38001f72cec908db722f616164bcd13a83
Rajkumar Purushothaman <raj.dhandus@gmail.com>
2017-04-08 22:02:46 -0400

    boolean and char

---
+++ CharAndBoolean/src/com/raj/Main.java
@@ -0,0 +1,18 @@
+package com.raj;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // width of 2 bytes; 16 bits of 1/0
+        char myChar ='a';
+        char copyRightSym ='\u00A9';
+        char registeredSym ='\u00AE';
+
+        System.out.println("copyRightSym : " + copyRightSym);
+        System.out.println("registeredSym : " + registeredSym);
+
+        boolean isFemale = false;
+        boolean isMale = true;
+
+    }
+}
