80e47fcdd8330f4257a82c041faff97fe6cb54a5
Rajkumar Purushothaman <raj.dhandus@gmail.com>
2017-04-08 21:32:39 -0400

    modified bad comments

--- ByteShortInt/src/Main.java
+++ ByteShortInt/src/Main.java
@@ -5,7 +5,7 @@ public class Main {
 
     public static void main(String[] args){
 
-        // integer has a width of 32; hence int's width is 2^32
+        // integer has a width of 4 bytes; 4*8=32 bits available for storing 1 or 0
 
         int myMinValue = -2_147_483_648;
         int myMaxValue = 2_147_483_647; // _ is a java 8 feature
@@ -13,7 +13,7 @@ public class Main {
         int myTotal = (myMinValue/2);
 
         System.out.println("myTotal : "+myTotal);
-        // byte has a width of 8; hence int's width is 2^8
+        // byte has a width of 1 byte; 1*8=8 bits available for storing 1 or 0
 
         byte myMinByteValue = -128;
         byte myMaxByteValue = 127;
@@ -22,7 +22,7 @@ public class Main {
 
         System.out.println("myNewByteValue : "+myNewByteValue);
 
-        // short has a width of 16; hence int's width is 2^16
+        // short has a width of 2 bytes; 2*8=16 bits available for storing 1 or 0
 
         short myMinShortValue = -32768;
         short myMaxShortValue = 32767;
@@ -31,7 +31,7 @@ public class Main {
 
         System.out.println("myNewShortValue : " + myNewShortValue);
 
-        // long has a width of 64; hence int's width is 2^64
+        // long has a width of 8; 8*8=64 bits available for storing 1 or 0
 
         long myMinLongValue = -9_223_372_036_854_775_808L; // the suffix can be small 'l' or capital 'L'
         long myMaxLongValue = 9_223_372_036_854_775_807L;
--- FloatAndDouble/src/com/company/Main.java
+++ FloatAndDouble/src/com/company/Main.java
@@ -4,11 +4,34 @@ public class Main {
 
     public static void main(String[] args) {
 
-        int myIntValue = 5;
-        float myFloatValue = 5.4f;
-        double myDoubleValue = 5.4;
+        //width of an int = 4 bytes or 32 bits
+        int myIntValue = 5/3;
 
+        //width of a float = 4 bytes or 32 bits
+        float myFloatValue = 5/3;
 
+        //float myFloatValue1 = 5.2; --> will cause a compilation error;
+        // java assumes literal decimal as double; assigning it to float will cause error
+
+        //byte temp = 127;
+        //byte temp1 = temp/2; --> will cause a compilation error;
+        // java assumes literal expressions as int; assigning it to short, byte, long will cause error
+
+        // can also be written as 5f/2f
+        //float has 7 digits of precision
+
+        //width of a float = 8 bytes or 64 bits
+        double myDoubleValue = 5d/3;
+        // can also be written as 5d/2d
+        //double has 16 digits of precision
+
+        //double is faster on modern computers
+        // inbuilt functions in java return double
+        // more precise
+
+        System.out.println(myIntValue);
+        System.out.println(myFloatValue);
+        System.out.println(myDoubleValue);
 
     }
 }
