fa45f673a0a58bab65b59d8ee30d72635e1d0c20
Rajkumar Purushothaman <raj.dhandus@gmail.com>
2017-04-27 20:49:58 -0400

    Inheritance

--- Interfaces/src/com/raj/Ford.java
+++ Interfaces/src/com/raj/Ford.java
@@ -3,20 +3,19 @@ package com.raj;
 /**
  * Created by rajkumarpurushothaman on 4/27/17.
  */
-public class BMW implements ICar, IPolishable{
-
+public class Ford implements ICar, IPolishable{
     @Override
     public void driveForward() {
-        System.out.println("Driving BMW forward!");
+        System.out.println("Driving Ford forward!");
     }
 
     @Override
     public void driveBackward() {
-        System.out.println("Driving BMW backwards");
+        System.out.println("Driving Ford backward!");
     }
 
     @Override
     public void polish() {
-        System.out.println("Polishing BMW");
+        System.out.println("Polishing Ford");
     }
 }
--- Interfaces/src/com/raj/Main.java
+++ Interfaces/src/com/raj/Main.java
@@ -4,5 +4,16 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+        ICar car = new BMW();
+        car.driveBackward();
+        car.driveForward();
+        IPolishable polishable = (IPolishable) car;
+        ((IPolishable) car).polish();
+
+        car = new Ford();
+        ((IPolishable) car).polish();
+        car.driveForward();
+        car.driveBackward();
+
     }
 }
