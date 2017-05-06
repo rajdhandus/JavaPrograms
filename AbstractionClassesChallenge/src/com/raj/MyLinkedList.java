8bf58c4211bb4062c5b4fec069a272f6602a94cd
Rajkumar Purushothaman <raj.dhandus@gmail.com>
2017-05-06 19:28:28 -0400

    Abstraction Challenge continued

--- AbstractionClassesChallenge/src/com/raj/MyLinkedList.java
+++ AbstractionClassesChallenge/src/com/raj/MyLinkedList.java
@@ -23,50 +23,47 @@ public class MyLinkedList implements NodeList {
             return true;
         }
 
-        Item currentItem = this.root;
+        Item current = getRoot();
 
-        while(currentItem!=null) {
-            int compareResult = currentItem.compareTo(item);
-            if(compareResult <0) {
-                //item is larger, move right and insert
-                if(currentItem.next()!=null) {
-                    currentItem = currentItem.next();
+        while(current!=null) {
+            int compareResult = current.compareTo(item);
+            if(compareResult>0) {
+                // item is small, move left
+                if(current.previous()!=null) {
+                    Item prev = current.previous();
+
+                    item.setPrevious(prev);
+                    prev.setNext(item);
+                    
+                    item.setNext(current);
+                    current.setPrevious(item);
+
+                    return true;
                 }
                 else {
-                    currentItem.setNext(item);
-                    item.setPrevious(currentItem);
+                    this.root.setPrevious(item);
+                    this.root = item;
+                    item.setNext(this.root);
                     return true;
                 }
             }
-            else if(compareResult>0) {
-                // item is smaller, move left and insert
-                if(currentItem.previous()!=null) {
-                    Item prev = currentItem.previous();
-                    prev.setNext(item);
-                    item.setPrevious(prev);
-                    item.setNext(currentItem);
-                    currentItem.setPrevious(item);
+            else if(compareResult<0) {
+                // item is bigger, move right
+                if(current.next()!=null) {
+                    current = current.next();
                 }
                 else {
-                    // we have reached the first item a.k.a this.root
-                    // so instead of currentItem use this.root
-
-//                    currentItem.setPrevious(item);
-//                    item.setNext(currentItem);
-
-                    this.root.setPrevious(item);
-                    item.setNext(this.root);
-                    this.root = item;
+                    current.setNext(item);
+                    item.setPrevious(current);
+                    return true;
                 }
-                return true;
             }
             else {
-                // duplicate
-                System.out.println("Deplicate item found!");
+                // item is already present, ignore!
+                System.out.println("Duplicate item");
                 return false;
             }
         }
-
         return false;
     }
 
