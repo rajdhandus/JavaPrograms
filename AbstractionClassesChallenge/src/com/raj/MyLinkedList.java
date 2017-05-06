3338abee2b935d341a82732f2c72b125733c96e3
Rajkumar Purushothaman <raj.dhandus@gmail.com>
2017-05-06 10:28:54 -0400

    Abstraction Challenge continued

--- AbstractionClassesChallenge/src/com/raj/MyLinkedList.java
+++ AbstractionClassesChallenge/src/com/raj/MyLinkedList.java
@@ -18,6 +18,55 @@ public class MyLinkedList implements NodeList {
 
     @Override
     public boolean addItem(Item item) {
+        if(this.root==null) {
+            this.root = item;
+            return true;
+        }
+
+        Item currentItem = this.root;
+
+        while(currentItem!=null) {
+            int compareResult = currentItem.compareTo(item);
+            if(compareResult <0) {
+                //item is larger, move right and insert
+                if(currentItem.next()!=null) {
+                    currentItem = currentItem.next();
+                }
+                else {
+                    currentItem.setNext(item);
+                    item.setPrevious(currentItem);
+                    return true;
+                }
+            }
+            else if(compareResult>0) {
+                // item is smaller, move left and insert
+                if(currentItem.previous()!=null) {
+                    Item prev = currentItem.previous();
+                    prev.setNext(item);
+                    item.setPrevious(prev);
+                    item.setNext(currentItem);
+                    currentItem.setPrevious(item);
+                }
+                else {
+                    // we have reached the first item a.k.a this.root
+                    // so instead of currentItem use this.root
+
+//                    currentItem.setPrevious(item);
+//                    item.setNext(currentItem);
+
+                    this.root.setPrevious(item);
+                    item.setNext(this.root);
+                    this.root = item;
+                }
+                return true;
+            }
+            else {
+                // duplicate
+                System.out.println("Deplicate item found!");
+                return false;
+            }
+        }
+
         return false;
     }
 
@@ -28,6 +77,10 @@ public class MyLinkedList implements NodeList {
 
     @Override
     public void traverse(Item root) {
-
+        Item current = root;
+        while(current.next()!=null) {
+            System.out.println("Value : " + current.getData());
+            current = current.next();
+        }
     }
 }
